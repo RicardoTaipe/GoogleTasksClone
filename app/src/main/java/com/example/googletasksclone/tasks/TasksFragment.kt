14dcb622537e59d4b73e87b6caedb0257d846300
RicardoTaipe <rdavid28@hotmail.es>
2024-08-24 21:03:16 -0500

    add add task ui screen

--- app/src/main/java/com/example/googletasksclone/tasks/TasksFragment.kt
+++ app/src/main/java/com/example/googletasksclone/tasks/TasksFragment.kt
@@ -1,31 +1,40 @@
 package com.example.googletasksclone.tasks
 
-import androidx.fragment.app.viewModels
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.googletasksclone.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.googletasksclone.addtask.AddTasksFragment
+import com.example.googletasksclone.databinding.FragmentTasksBinding
 
 class TasksFragment : Fragment() {
+    private var _binding: FragmentTasksBinding? = null
+    private val binding get() = _binding!!
 
     companion object {
         fun newInstance() = TasksFragment()
     }
 
     private val viewModel: TasksViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.addTasksButton.setOnClickListener {
+            val modalBottomSheet = AddTasksFragment()
+            modalBottomSheet.show(parentFragmentManager, AddTasksFragment::class.java.simpleName)
+        }
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.fragment_tasks, container, false)
+        _binding = FragmentTasksBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
