9d1662c839b2186ea6c1c7bd8b50c56df8789927
RicardoTaipe <rdavid28@hotmail.es>
2024-09-07 21:38:07 -0500

    add theme for compose

--- app/src/main/java/com/example/googletasksclone/MainActivity.kt
+++ app/src/main/java/com/example/googletasksclone/MainActivity.kt
@@ -1,173 +1,47 @@
 package com.example.googletasksclone
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.navigation.ui.AppBarConfiguration
-import com.example.googletasksclone.addtask.AddTasksFragment
-import com.example.googletasksclone.databinding.ActivityMainBinding
-import com.example.googletasksclone.home.TasksCollectionAdapter
-import com.example.googletasksclone.moreoptions.MoreOptionsEvent
-import com.example.googletasksclone.moreoptions.MoreOptionsFragment
-import com.example.googletasksclone.newlist.NewListFragment
-import com.example.googletasksclone.sort.SortEvent
-import com.example.googletasksclone.sort.SortFragment
-import com.example.googletasksclone.switchlist.SwitchEvent
-import com.example.googletasksclone.switchlist.SwitchListFragment
-import com.google.android.material.tabs.TabLayoutMediator
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.googletasksclone.ui.theme.TasksAppTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var tasksCollectionAdapter: TasksCollectionAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        setSupportActionBar(binding.toolbar)
-
-//        val navController = findNavController(R.id.nav_host_fragment_content_main)
-//        appBarConfiguration = AppBarConfiguration(navController.graph)
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-
-//        binding.fab.setOnClickListener { view ->
-//
-//        }
-        tasksCollectionAdapter = TasksCollectionAdapter(this).also {
-            binding.contentMain.tasksLists.adapter = it
-        }
-        renderTitlesInTabLayout()
-        handleCustomTabAction()
-        handleBottomBarActions()
-        navigateToAddTasksFragment()
-    }
-
-    private fun renderTitlesInTabLayout() {
-        TabLayoutMediator(
-            binding.contentMain.tabLayout, binding.contentMain.tasksLists
-        ) { tab, position ->
-            when (position) {
-                0 -> {
-                    tab.icon = ContextCompat.getDrawable(this, R.drawable.ic_star_24)
-                }
-
-                else -> {
-                    tab.text = "LIST ${(position)}"
+        setContent {
+            TasksAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
                 }
             }
-        }.attach()
-    }
-
-    private fun handleCustomTabAction() {
-        binding.contentMain.tabLayout.apply {
-            addCustomTab(getString(R.string.new_list))
-            setOnCustomTabSelectedListener {
-                navigateToNewListFragment()
-//                Snackbar.make(binding.root, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                    .setAction("Action", null).setAnchorView(binding.addTasksButton).show()
-            }
         }
+
     }
 
-    private fun handleBottomBarActions() {
-        binding.bottomAppBar.setOnMenuItemClickListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.switch_lists -> {
-                    navigateToSwitchListsFragment()
-                    true
-                }
+}
 
-                R.id.sort_tasks -> {
-                    navigateToSortFragment()
-                    true
-                }
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!", modifier = modifier
+    )
+}
 
-                R.id.more_options -> {
-                    navigateToMoreOptionsDialog()
-                    true
-                }
-
-                else -> false
-            }
-        }
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TasksAppTheme {
+        Greeting("Android")
     }
-
-    private fun navigateToSortFragment() {
-        SortFragment().apply {
-            onListItemSelected = {
-                when (it) {
-                    SortEvent.Date -> {}
-                    SortEvent.MyOrder -> {}
-                    SortEvent.Starred -> {}
-                }
-
-            }
-            show(supportFragmentManager, SortFragment.TAG)
-        }
-    }
-
-    private fun navigateToMoreOptionsDialog() {
-        MoreOptionsFragment().apply {
-            onListItemSelected = {
-                when (it) {
-                    MoreOptionsEvent.DeleteAllCompletedTasks -> {}
-                    MoreOptionsEvent.DeleteList -> {}
-                    MoreOptionsEvent.RenameList -> {}
-                }
-
-            }
-            show(supportFragmentManager, MoreOptionsFragment.TAG)
-        }
-    }
-
-    private fun navigateToSwitchListsFragment() {
-        SwitchListFragment().apply {
-            onListItemSelected = {
-                when (it) {
-                    is SwitchEvent.ItemSelected -> {
-                        binding.contentMain.tasksLists.setCurrentItem(it.id.toInt(), false)
-                        this.dismiss()
-                    }
-                }
-            }
-            show(supportFragmentManager, SwitchListFragment.TAG)
-        }
-    }
-
-    private fun navigateToAddTasksFragment() {
-        binding.addTasksButton.setOnClickListener {
-            AddTasksFragment().show(supportFragmentManager, AddTasksFragment.TAG)
-        }
-    }
-
-    private fun navigateToNewListFragment() {
-        NewListFragment().show(supportFragmentManager, NewListFragment.TAG)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-
-//    override fun onSupportNavigateUp(): Boolean {
-//        val navController = findNavController(R.id.nav_host_fragment_content_main)
-//        return navController.navigateUp(appBarConfiguration)
-//                || super.onSupportNavigateUp()
-//    }
 }
