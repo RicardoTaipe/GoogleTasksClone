36a6485cbcc0ec5751c594201f365a081ec6f5d5
RicardoTaipe <rdavid28@hotmail.es>
2025-03-18 00:36:30 -0500

    add category to room

--- app/src/main/java/com/example/googletasksclone/data/Task.kt
+++ app/src/main/java/com/example/googletasksclone/data/Task.kt
@@ -2,16 +2,27 @@ package com.example.googletasksclone.data
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 import java.util.*
 
-@Entity(tableName = "tasks")
+@Entity(
+    tableName = "tasks", foreignKeys = [
+        ForeignKey(
+            entity = Category::class,
+            parentColumns = ["id"],
+            childColumns = ["category_id"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class Task(
     @ColumnInfo(name = "title") var title: String = "",
     @ColumnInfo(name = "description") var description: String = "",
     @ColumnInfo(name = "completed") var isCompleted: Boolean = false,
     @PrimaryKey @ColumnInfo(name = "entry_id") var id: String = UUID.randomUUID().toString(),
-    @ColumnInfo(name = "date") var date: Long = System.currentTimeMillis()
+    @ColumnInfo(name = "date") var date: Long = System.currentTimeMillis(),
+    @ColumnInfo(name = "category_id") var categoryId: Long? = null
 ) {
     val titleForList: String
         get() = title.ifEmpty { description }
