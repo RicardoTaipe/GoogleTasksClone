37017eb73229cf7ffd5b9921408e0fcf94a73a89
RicardoTaipe <rdavid28@hotmail.es>
2025-08-25 00:30:24 -0500

    favorite dao

--- app/src/main/java/com/example/googletasksclone/starred/StarredTasksFragment.kt
+++ app/src/main/java/com/example/googletasksclone/starred/StarredTasksFragment.kt
@@ -20,3 +20,21 @@ class StarredTasksFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_starred_tasks, container, false)
     }
 }
+/*
+* val categoryDao = db.categoryDao()
+val taskDao = db.taskDao()
+
+// Obtener la categoría favorita actual
+val favoriteCategory = categoryDao.getFavoriteCategory()
+
+// Si ya hay una categoría favorita, desmarcarla
+if (favoriteCategory != null) {
+    categoryDao.resetFavoriteCategory()
+}
+
+// Establecer una nueva categoría como favorita
+categoryDao.setFavoriteCategory(newCategoryId)
+
+// Agregar tareas a la nueva categoría favorita
+val task = Task(title = "Nueva tarea", description = "Descripción de tarea", categoryId = newCategoryId)
+taskDao.insertTask(task)*/
